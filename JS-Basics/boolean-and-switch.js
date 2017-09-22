007bc5080f07a0cfcd6aad7a631dd546fa28aaed
moumensoliman <moumensoliman@gmail.com>
2017-09-22 13:26:12 +0200

    JS-Basics

---
+++ JS-Basics/boolean-and-switch.js
@@ -0,0 +1,28 @@
+var age = 20;
+
+if (age < 20) {
+    console.log('John is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log('John is a young man.');
+} else {
+    console.log('John is a man.');
+}
+
+
+var job = 'teacher';
+
+job = prompt('What does john do?');
+
+switch (job) {
+    case 'teacher':
+        console.log('John teaches kids.');
+        break;
+    case 'driver':
+        console.log('John drives a cab in Lisbon.');
+        break;
+    case 'cop':
+        console.log('John helps fight crime.');
+        break;
+    default:
+        console.log('John does something else.');
+}
