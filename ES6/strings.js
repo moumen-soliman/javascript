3c04ed42c081decef41d746f3552c4e8063d5c37
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 23:54:40 +0200

    Strings

---
+++ ES6/strings.js
@@ -0,0 +1,20 @@
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('Sm'));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
