94d88cbb9c4f223c44b293366f1b2c88c87d0721
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 23:31:08 +0200

    let and const

---
+++ ES6/let-and-const.js
@@ -0,0 +1,54 @@
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+
+
+// ES5
+function driversLicence5(passedTest) {
+    
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    
+    
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence5(true);
+
+
+// ES6
+function driversLicence6(passedTest) {
+    
+    //console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
+    
+    if (passedTest) {
+        firstName = 'John';
+    }
+    
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence6(true);
+
+
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
