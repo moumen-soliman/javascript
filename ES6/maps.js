7643dfbba2697ca1d1beecd80e0356c5f4947d3a
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-27 00:49:36 +0200

    Maps

---
+++ ES6/maps.js
@@ -0,0 +1,33 @@
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get('question'));
+//console.log(question.size);
+
+
+if(question.has(4)) {
+    //question.delete(4);
+    //console.log('Answer 4 is here')
+}
+
+//question.clear();
+
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
