74b2dbf5fd9542b1512d1cef0d9ea4041bf78ef7
Mo'men Soliman <moumensoliman@gmail.com>
2018-01-26 23:43:43 +0200

    Arrow Function

---
+++ ES6/arrow-functions.js
@@ -0,0 +1,22 @@
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
